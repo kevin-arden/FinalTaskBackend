62cc5e95e70fb10881c9fd101905e1aa6c4d6aff
Kevin Arden Islamey <kevinarden.islamey@gmail.com>
2021-03-22 14:00:07 +0700

    edit migrations

--- migrations/20210216013657-create-transactions.js
+++ migrations/20210216013657-create-transactions.js
@@ -1,39 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Transactions', {
+    await queryInterface.createTable("Transactions", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      userId: {
-        type: Sequelize.INTEGER
+      usersId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
       },
       attachment: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       totalPayment: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       bookCart: {
-        type: Sequelize.JSON
+        type: Sequelize.JSON,
       },
       status: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Transactions');
-  }
+    await queryInterface.dropTable("Transactions");
+  },
 };
